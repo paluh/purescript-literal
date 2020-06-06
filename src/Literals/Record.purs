d61c56e1089141a6fb5581b93ab6e8cba414ac24
Tomasz Rybarczyk <paluho@gmail.com>
2020-06-06 11:20:18 +0200

    Cleanup Record module

--- src/Literals/Record.purs
+++ src/Literals/Record.purs
@@ -8,16 +8,3 @@ import Type.RowList (RLProxy(..))
 
 type RecordLit r = Literal (RProxy r) (RProxy r)
 
--- class Record (head :: Symbol) (tail :: Symbol) (part :: NumberPart)
--- 
--- instance signPart :: (Cons h' t' t, Number' h' t' IntegerPart) => Number' "-" t Sign
--- else instance signPartEmpty :: (Number' h t IntegerPart) => Number' h t Sign
--- else instance pointIntegerPart :: (Cons h' t' t, Number' h' t' FractionalPart) => Number' "." t IntegerPart
--- else instance lastFractionalPart :: (Digit h) => Number' h "" FractionalPart
--- else instance digitIntegerPart :: (Digit h, Cons h' t' t, Number' h' t' IntegerPart) => Number' h t IntegerPart
--- else instance digitFractionalPart :: (Digit h, Cons h' t' t, Number' h' t' FractionalPart) => Number' h t FractionalPart
-
--- r = prop (SProxy ∷ SProxy "test")
-
--- recordLit :: forall sym. IsSymbol sym => Number sym => RecordLit r
--- recordLit = reflect r
